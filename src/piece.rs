a2a3e8ad717620a1e2860e488827cdd007883787
Dworv <dwarvyt@gmail.com>
2023-03-06 22:34:28 -0800

    feat: more funcitons on piece trait

--- src/piece.rs
+++ src/piece.rs
@@ -1,8 +1,10 @@
 use crate::{Board, Step};
 
-pub trait Piece: Sized {
-    fn forwards(&self) -> (i8, i8);
-    fn sideways(&self) -> (i8, i8);
+pub trait Piece: Copy + Sized {
+    fn forwards(&self) -> (u16, u16);
+    fn sideways(&self) -> (u16, u16);
     fn can_kill(&self, other: &Self) -> bool;
+    fn is_king(&self) -> bool;
     fn possible_steps(&self, board: &Board<Self>) -> dyn Iterator<Item = Step>;
+    fn from_fen_char() -> Option<Self>;
 }
