6a730496797d8cca44a35cb7e8f26724fddd1ef6
Dworv YT <dwarvyt@gmail.com>
2023-03-06 16:42:55 -0800

    tests: update integration tests

--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -4,13 +4,10 @@ use sandgoby;
 #[ignore = "not done yet"]
 #[test]
 fn basic_test() {
-    let board = sandgoby::BoardBuilder::new()
-        .with_piece_set(sandgoby::ClassicPieces)
-        .with_colours(sandgoby::BW)
-        .with_rules(sandgoby::ClassicRules)
-        .fill(Fen::new("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"))
-        .build()
-        .unwrap();
+    let board: sandgoby::Board<sandgoby::ClassicPieces> = sandgoby::Board::assemble(
+        Fen::compile("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1").unwrap(),
+        |square| square.0 < 8 && square.0 >= 0 && square.1 < 8 && square.1 >= 0
+    ).unwrap();
     board.get_status();
     let mut moves = board.get_moves();
     board.make_move(moves.next());
