760babc7162e563ef1f66c80d7d47b5717a48ef6
Dworv <dwarvyt@gmail.com>
2023-03-10 07:54:26 -0800

    feat: more fields on piece trait

--- src/piece.rs
+++ src/piece.rs
@@ -1,10 +1,12 @@
-use crate::{Board, Step};
+use crate::{Board, Step, square::Direction, Square};
 
 pub trait Piece: Copy + Sized {
     const NUM_TEAMS: u16;
+    const BOARD_SIZE: (u16, u16);
     type StepIter: Iterator<Item = Step>;
-    fn forwards(&self) -> (u16, u16);
-    fn sideways(&self) -> (u16, u16);
+    fn in_bounds(square: Square) -> bool;
+    fn forwards(&self) -> Direction;
+    fn sideways(&self) -> Direction;
     fn can_kill(&self, other: &Self) -> bool;
     fn is_king(&self) -> bool;
     fn team_id(&self) -> u16;
